dc47ab806792507565c893c2f350b6f34a321205
Sam Fishman <sfishman@college.harvard.edu>
2014-10-05 03:20:43 -0400

    add wifi_location to package.json for client

--- client/client.js
+++ client/client.js
@@ -1,5 +1,6 @@
 var JSFtp = require("jsftp");
 var fs = require('fs');
+var wifiLocation = require('wifi_location');
 
 var iocli = require('socket.io-client'),
 socketcli = iocli.connect('192.168.1.2', {
